c34805b9ba6d4773d7f236b3189e0606065b3c38
los <thiscarlos@gmail.com>
2023-01-26 14:52:33 -0800

    aboreto added as main font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import '@/styles/globals.css'
-import { Roboto } from '@next/font/google'
+import { Roboto, Aboreto } from '@next/font/google'
 
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '700']
 })
 
+const aboreto = Aboreto({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
 export default function App({ Component, pageProps }) {
   return (
-    <main className={roboto.className}>
+    <main className={aboreto.className}>
       <Component {...pageProps} />
     </main>
+  )
 }
